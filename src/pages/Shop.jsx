a11a5bfa82b1fca24f6fca9a6144d2ea9a6c0d1f
JakubSieron-Git <thyned@gmail.com>
2025-04-13 19:58:56 +0100

    pictures

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,42 +3,105 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Shop.scss';
 
+import dwarfImg from '../assets/images/bat1.jpg';
+import elfImg from '../assets/images/bat2.jpg';
+import terrainImg from '../assets/images/ter1.jpg';
+import paintsBasicImg from '../assets/images/pset2.jpg';
+import bretImg from '../assets/images/bat3.jpg';
+import paintsAdvancedImg from '../assets/images/pset1.jpg';
+import terrain2Img from '../assets/images/pset3.jpg';
+import hillImg from '../assets/images/ter2.jpg';
+import manorImg from '../assets/images/ter3.jpg';
+import chaoImg from '../assets/images/bat4.jpg';
+import dwfImg from '../assets/images/bat5.jpg';
+import setImg from '../assets/images/bat6.jpg';
+import set3Img from '../assets/images/bat7.jpg';
+import arcImg from '../assets/images/bat8.jpg';
+
 const dummyProducts = [
   {
     id: 1,
-    name: 'Orc Warrior',
+    name: 'Dwarf Battalion',
     category: 'miniatures',
-    image: 'https://i.imgur.com/sW1VZIS.png',
+    image: dwarfImg,
   },
   {
     id: 2,
-    name: 'Elven Archer',
+    name: 'Elven Battalion',
     category: 'miniatures',
-    image: 'https://i.imgur.com/8IFL1aZ.png',
+    image: elfImg,
   },
   {
     id: 3,
-    name: 'Fantasy Terrain Set',
+    name: 'Arcane Ruins',
     category: 'terrain',
-    image: 'https://i.imgur.com/GqHc3vW.png',
+    image: terrainImg,
   },
   {
     id: 4,
     name: 'Basic Paints Pack',
     category: 'paints',
-    image: 'https://i.imgur.com/Uj7QZGn.png',
+    image: paintsBasicImg,
   },
   {
     id: 5,
-    name: 'Dark Wizard',
+    name: 'Bretonia Starter Set',
     category: 'miniatures',
-    image: 'https://i.imgur.com/ZVTXcb0.png',
+    image: bretImg,
   },
   {
     id: 6,
     name: 'Advanced Paint Set',
     category: 'paints',
-    image: 'https://i.imgur.com/z7fPzJH.png',
+    image: paintsAdvancedImg,
+  },
+  {
+    id: 7,
+    name: 'Advanced Terrain Set',
+    category: 'paints',
+    image: terrain2Img,
+  },
+  {
+    id: 8,
+    name: 'Hills',
+    category: 'terrain',
+    image: manorImg,
+  },
+  {
+    id: 9,
+    name: 'Fortified Manor',
+    category: 'terrain',
+    image: hillImg,
+  },
+  {
+    id: 10,
+    name: 'Chaos Warriors',
+    category: 'miniatures',
+    image: chaoImg,
+  },
+  {
+    id: 11,
+    name: 'Dwarf Thunderers',
+    category: 'miniatures',
+    image: dwfImg,
+  },
+  {
+    id: 12,
+    name: 'Hexbane Hunters Set',
+    category: 'miniatures',
+    image: setImg,
+  },
+  {
+    id: 13,
+    name: '3 Sets',
+    category: 'miniatures',
+    image: set3Img,
+  },
+  {
+    id: 14,
+    name: 'Kharadron Overlords',
+    category: 'miniatures',
+    image: arcImg,
   },
 ];
 
